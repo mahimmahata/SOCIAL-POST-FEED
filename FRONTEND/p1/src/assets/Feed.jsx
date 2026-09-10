feb9bb2db9a5390165a080f1ff38c9fd78f9115c
mahimmahata <mahimmahata9990@gmail.com>
2026-09-10 08:56:17 +0530

    added confirmation popup

--- FRONTEND/p1/src/assets/Feed.jsx
+++ FRONTEND/p1/src/assets/Feed.jsx
@@ -19,6 +19,8 @@ const Feed = () => {
     }, [])
 
     const handelDelete = async (postId) => {
+        const confirmDelete = window.confirm("want to delete the post ?")
+        if(!confirmDelete) return
         try{
             await axios.delete(`http://localhost:100/DeletePost/${postId}`);
             setPosts( (prevPosts) => 
